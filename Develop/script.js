65d8bf0cd3a240e8abaac48c3118f12b4cb16405
cici <ci.gauntt@gmail.com>
2022-05-05 22:30:20 -1000

    restarted

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,15 @@
 // Assignment code here
-const specialCharacters = "!@#$%^&*(){}[]<>/?"
-const generateButton = document.getElementById('generateBtn')
+var passwordLength;
+var confirmLower;
+var confirmUpper;
+var confirmNumber;
+var confirmSpecial;
+var userChoices;
+
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var number = [0,1,2,3,4,5,6,7,8,9];
+var special = "!@#$%^&*()[]{}<>,.?/";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -17,44 +26,4 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Prompts that display after you click 'Generate Password'
-function generatePassword() {
-  var passwordLength = prompt ("Enter number of characters for your new password. It must be at least 8 characters, but no more than 128 characters.");
-  
-  var numbers = confirm ("Would you like numbers in your password?");
-
-  var lowerCase = confirm ("Would you like lower case letters in you password?");
-
-  var upperCase = confirm ("Would you like upper case letters in your password?");
-
-  var special = confirm ("Would you like special characters in your password?");
-
-  var minimumCount = 0;
-
-  var minimumNumbers = "";
-  var minimumLowerCases = "";
-  var minimumUpperCases = "";
-  var minimumSpecialCharacters = "";
-
-  function getNumbers() {
-    return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
-    }
-
-  function getLowerCase() {
-      return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
-    }
-
-  function getUpperCase() {
-      return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
-    }
-
-  function getSpecialCharacters() {
-    const specialCharacters = "!@#$%^&*()[]{}<>?"
-    return specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
-    }
-
-    console.log(getNumbers());
-    console.log(getLowerCase());
-    console.log(getUpperCase());
-    console.log(getSpecialCharacters());
 }
